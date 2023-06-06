ec4f9ea378af08ba9edd58e642c6df162878e991
Jason Triemert <jaytriemert@gmail.com>
2023-06-05 19:49:38 -0500

    added base react wireframe

---
+++ client/src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <section class="sidebar" id="contact">
+      <figure>
+        <img id="me" src="assets/images/profilepic.jpg" alt="my profile picture"/>
+        <h2>Jason Triemert</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo provident dicta sed ratione expedita perferendis reiciendis, ea fugit earum, quae, ducimus nostrum veritatis nemo aliquam? Odio, sapiente! Voluptates, consequatur dolores.</p>
+      </figure>
+      <p>Contact Info:</p>
+      <p>Phone: 612.555.8263</p>
+      <p>Email: jaytriemert@gmail.com</p>
+    </section>
+  );
+}
